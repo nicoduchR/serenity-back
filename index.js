64de85070fd4dadab12826b860ee05705b06d7ef
Duchemann <nicolas.duchemann@redlab.io>
2020-02-13 22:35:33 +0100

    Prepare login access to back-end

--- index.js
+++ index.js
@@ -24,11 +24,51 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json())
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+/**
+ * --------------------------
+ * Retrieve credentials : JWT token 
+ * --------------------------
+ */
+app.post('/api-0.1/login', (req, res, next) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    //checking to make sure the user entered the correct username/password combo
+    if(username === USERNAME && password === PASSWORD) { 
+        const user = {
+            username: USERNAME,
+            password: PASSWORD,
+        };
+        //if user log in success, generate a JWT token for the user with a secret key
+        jwt.sign(user, PRIVATE_KEY_JWT, { expiresIn: '24h' },(err, token) => {
+            if(err) { console.log(err) }    
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({jwt: token}, null, 3));
+        });
+    } else {
+        console.log('ERROR: Could not log in');
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({jwt: null}, null, 3));
+        // res.sendStatus(403)
+    }
+})
 
-
+/**
+ * --------------------------
+ * Check if JWT token is valid 
+ * --------------------------
+ */
+const checkToken = function (req,res,next){
+    const header = req.headers['authorization'];
+    if(typeof header !== 'undefined')
+    {
+       const bearer = header.split(' ');
+       const token = bearer[1];
+       req.token=token;
+       next();
+    }else{
+        res.sendStatus(403);
+    }
+}
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
